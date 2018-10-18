d736cac19e477381c61b442c895f6150a81e5791
Kofi <expl00ra@protonmail.com>
2018-10-18 16:28:20 +0100

    miscellaneous

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Tweet, Reply, Like
 
 # Register your models here.
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ('body', 'author', 'date_created')
+
+class ReplyAdmin(admin.ModelAdmin):
+    list_display = ('body', 'author', 'date_created')
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('tweet', 'author', 'date_created')
+
+admin.site.register(Tweet, TweetAdmin)
+admin.site.register(Reply, TweetAdmin)
+admin.site.register(Like, LikeAdmin)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tweet(models.Model):
+    """
+    Represents the tweet object
+    """
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=140)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_created',)
+
+    def __str__(self):
+        return self.body
+
+
+class Reply(models.Model):
+    """
+    Represents the Reply object
+    """
+    tweet = models.ForeignKey(Tweet, related_name='replies', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=140)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_created',)
+    
+
+    def __str__(self):
+        return self.body
+
+
+class Like(models.Model):
+    """
+    Represents Likes given to Tweet and Reply 
+    """
+    tweet = models.ForeignKey(Tweet, related_name='likes', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, related_name='likes', on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_created',)
+
+    def __str__(self):
+        return '{} liked {}'.format(self.author, self.tweet)
